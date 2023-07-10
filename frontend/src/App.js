062b2cbc7fc333124653a82d6c4b39ac6447b78c
Siddharthmishraweb <89569473+smishra-facilgo@users.noreply.github.com>
2023-07-10 18:51:47 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from '../src/screens/HomeScreen'
+import { Outlet } from 'react-router-dom'
+
+
+const App = () => {
+  return (
+     <>
+        <Header/>
+        <main className='py-3'>
+          <Container>
+            <h1> Welcome to ProShop </h1>
+            <Outlet />
+          </Container>
+        </main>
+        <Footer />
+     </>
+  );
+}
+
+export default App;
