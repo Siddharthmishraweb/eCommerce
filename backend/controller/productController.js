b83f07c09251b1463314d404eef07e7ccbb4f058
Siddharthmishraweb <mishrasiddharth1999@gmail.com>
2023-07-29 18:46:16 +0530

    get products data from redux

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,12 +1,18 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
+// @desc Fetch all Product
+// @route GET /api/products
+// @access Public
 const getProducts = asyncHandler(async (req, res) => {
    const products = await Product.find({})
    console.log('product api me aaya h')
    res.json(products);
 })
 
+// @desc Fetch a Product
+// @route GET /api/products/:id
+// @access Public
 const getProductById = asyncHandler(async (req, res) => {
    const product = await Product.findById(req.params.id);
    if(product)
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,32 +4,43 @@ import { useEffect, useState } from 'react';
 import Product from '../components/Product'
 import ProductScreen from '../screens/ProductScreen';
 import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
-   const [products, setProducts] = useState([]);
+   // const [products, setProducts] = useState([]);
 
-   useEffect(() => {
-      const fetchProduct = async () => {
-         const { data } = await axios.get('/api/products');
-         setProducts(data);
-      }
-      fetchProduct();
-   }, [])
- 
+   // useEffect(() => {
+   //    const fetchProduct = async () => {
+   //       const { data } = await axios.get('/api/products');
+   //       setProducts(data);
+   //    }
+   //    fetchProduct();
+   // }, [])
+   
+   const { data: products , isLoading, error } = useGetProductsQuery();
+
+
    console.log("prod**** ",products);
    return(
       <div>
-         <h1>Latest Products</h1>
-         <Row>
-            {
-               products.map((product) => (
-                  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                     {/* <h3>{product.name}</h3> */}
-                     <Product product={product}/>
-                  </Col>
-               ))
-            }
-         </Row>
+         { isLoading ? (
+            <h2> error </h2>
+         ) : error ? ( <div>{error?.data?.message || error ?.error}</div>) : (
+            <div>
+               <h1>Latest Products</h1>
+               <Row>
+                  {
+                     products.map((product) => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                           {/* <h3>{product.name}</h3> */}
+                           <Product product={product}/>
+                        </Col>
+                     ))
+                  }
+               </Row>            
+            </div>
+         )}
+
       </div>
    )
 }
