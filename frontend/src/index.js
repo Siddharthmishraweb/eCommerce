d3560d8754d0b0d87cf1d651fbd4f939338f2a52
Siddharthmishraweb <mishrasiddharth1999@gmail.com>
2023-07-29 17:30:41 +0530

    api slice and redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,10 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider
-} from 'react-router-dom'
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './assets/styles/bootstrap.custom.css';
+import store from './store'
 import './assets/styles/index.css';
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
@@ -25,7 +27,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router = {router}/>
+    <Provider store = {store}>
+      <RouterProvider router = {router}/>
+    </Provider>
   </React.StrictMode>
 );
 
