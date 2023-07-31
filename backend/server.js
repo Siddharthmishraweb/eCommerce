cf4128b11f73d60f397e84569f9b50075b227f6f
Siddharthmishraweb <mishrasiddharth1999@gmail.com>
2023-07-31 23:01:48 +0530

    partially completed auth user/login user

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,10 @@ connectDB() // starting mongodb
 
 const app = express();
 
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }))
+
 app.use(cors());
 
 
