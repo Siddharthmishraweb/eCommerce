1e6a7bb2bc9a2d071ff634c6ba0deb8ff319e3be
Siddharthmishraweb <89569473+smishra-facilgo@users.noreply.github.com>
2023-08-19 12:19:52 +0530

    PAYPAL SETUP

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,12 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes);
 
+console.log("PAYPAL_CLIENT_ID:  ",process.env);
+
+app.get('/api/config/paypal', (req, res) => {
+   clientId:  process.env.PAYPAL_CLIENT_ID
+})
+
 
 app.use(notFound)
 app.use(errorHandler)
