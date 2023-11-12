e920e1e6a16b9e4202648ddcf4ac623dba8a1168
Siddharthmishraweb <mishrasiddharth1999@gmail.com>
2023-11-12 09:18:28 +0530

    Feature added review backend

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -3,7 +3,7 @@ import express from "express";
 // import asyncHandler from "../middleware/asyncHandler.js";
 // import Product from "../models/productModel.js";
 // import productController from '../controller/productController.js'
-import { getProducts, getProductById, createProduct, updateProduct, deleteProduct } from "../controller/productController.js";
+import { getProducts, getProductById, createProduct, updateProduct, deleteProduct, createProductReview } from "../controller/productController.js";
 import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router()
 
@@ -15,5 +15,5 @@ const router = express.Router()
 // both are same
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
-
-export default router
+router.route('/:id/reviews').post(protect, createProductReview);
+export default router;
